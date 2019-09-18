2d14ac99929438e42d79182e39acc1c1c64c0dd3
Fabio Cagliero <caglierofabio@gmail.com>
2019-09-18 12:29:13 +0200

    First commit

---
+++ readme.md
@@ -0,0 +1,15 @@
+# Laravel active
+Helper class for Laravel applications to get active class based on current route.
+
+This package is still in development.
+
+## Installation
+```bash
+composer require webnuvola/laravel-active
+```
+
+## Credits
+- [Fabio Cagliero](https://github.com/fab120)
+
+## License
+The MIT License (MIT). Please see [License File](LICENSE) for more information.
---
+++ src/helpers.php
@@ -0,0 +1,72 @@
+<?php
+
+if (! function_exists('active_class')) {
+    /**
+     * Get the active class if the condition is not false.
+     *
+     * @param mixed $condition
+     * @param string $activeClass
+     * @param string $inactiveClass
+     * @return string
+     */
+    function active_class($condition, string $activeClass = 'active', string $inactiveClass = ''): string
+    {
+        return $condition ? $activeClass : $inactiveClass;
+    }
+}
+
+if (! function_exists('if_uri')) {
+    /**
+     * Determine if the current request URI matches a pattern.
+     *
+     * @param mixed ...$patterns
+     * @return bool
+     */
+    function if_uri(...$patterns): bool
+    {
+        return request()->is(...$patterns);
+    }
+}
+
+if (! function_exists('if_uri_pattern')) {
+    /**
+     * Determine if the current request URI matches a pattern.
+     *
+     * @see if_uri()
+     *
+     * @param mixed ...$patterns
+     * @return bool
+     */
+    function if_uri_pattern(...$patterns): bool
+    {
+        return request()->is(...$patterns);
+    }
+}
+
+if (! function_exists('if_route')) {
+    /**
+     * Determine if the route name matches a given pattern.
+     *
+     * @param mixed ...$patterns
+     * @return bool
+     */
+    function if_route(...$patterns): bool
+    {
+        return request()->routeIs(...$patterns);
+    }
+}
+
+if (! function_exists('if_route_pattern')) {
+    /**
+     * Determine if the route name matches a given pattern.
+     *
+     * @see if_route()
+     *
+     * @param mixed ...$patterns
+     * @return bool
+     */
+    function if_route_pattern(...$patterns): bool
+    {
+        return request()->routeIs(...$patterns);
+    }
+}
