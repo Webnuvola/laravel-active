877ea26c3ba79db6c25a882c14c820f66275df93
Fabio Cagliero <caglierofabio@gmail.com>
2020-05-15 14:25:24 +0200

    Add `if_route_param` function Fix docblock

--- src/helpers.php
+++ src/helpers.php
@@ -4,9 +4,9 @@ if (! function_exists('active_class')) {
     /**
      * Get the active class if the condition is not false.
      *
-     * @param mixed $condition
-     * @param string $activeClass
-     * @param string $inactiveClass
+     * @param  mixed $condition
+     * @param  string $activeClass
+     * @param  string $inactiveClass
      * @return string
      */
     function active_class($condition, string $activeClass = 'active', string $inactiveClass = ''): string
@@ -19,7 +19,7 @@ if (! function_exists('if_uri')) {
     /**
      * Determine if the current request URI matches a pattern.
      *
-     * @param mixed ...$patterns
+     * @param  mixed ...$patterns
      * @return bool
      */
     function if_uri(...$patterns): bool
@@ -34,7 +34,7 @@ if (! function_exists('if_uri_pattern')) {
      *
      * @see if_uri()
      *
-     * @param mixed ...$patterns
+     * @param  mixed ...$patterns
      * @return bool
      */
     function if_uri_pattern(...$patterns): bool
@@ -47,7 +47,7 @@ if (! function_exists('if_route')) {
     /**
      * Determine if the route name matches a given pattern.
      *
-     * @param mixed ...$patterns
+     * @param  mixed ...$patterns
      * @return bool
      */
     function if_route(...$patterns): bool
@@ -62,11 +62,25 @@ if (! function_exists('if_route_pattern')) {
      *
      * @see if_route()
      *
-     * @param mixed ...$patterns
+     * @param  mixed ...$patterns
      * @return bool
      */
     function if_route_pattern(...$patterns): bool
     {
         return request()->routeIs(...$patterns);
+    }
+}
+
+if (! function_exists('if_route_param')) {
+    /**
+     * Check if the parameter of the current route has the correct value.
+     *
+     * @param  string $param
+     * @param  mixed $value
+     * @return bool
+     */
+    function if_route_param(string $param, $value): bool
+    {
+        return request()->route($param) === $value;
     }
 }
